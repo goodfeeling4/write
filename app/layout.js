1464eee8e102cdcbcd6f9eae9c3c4a88f7e08bb6
md intsab alam <your-email@example.com>
2025-06-18 17:28:12 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
       <body 
       >
         <div className="flex flex-around items-center justify-between ">
-              <h1 className=" text-3xl text-[#004FFF] m-4">write</h1>
+              <a href="/" className=" text-3xl text-[#004FFF] m-4">write</a>
               <button className="bg-blue-500 text-white rounded p-2 hover:bg-blue-600 transition-colors m-4">
         <a href="/addTopic/123">Add Topic</a>
       </button>
