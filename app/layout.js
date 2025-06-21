eeb2daa1c6470a9116861c8ba57196dadf85dd85
Md shahnebaj <mdshahnebaj649@gmail.com>
2025-06-21 23:29:02 +0530

    header's margin

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
       <body
         className=""
       >
-        <div className="flex h-[10vh] items-center justify-between p-8 mb-0.5 dark:bg-[#111827] bg-blue-300">
+        <div className="flex h-[10vh] items-center justify-between sm:p-8 p-3 mb-0.5 dark:bg-[#111827] bg-blue-300">
           <div className="flex sm:gap-4 place-items-center sm:m-1" >
             <a href="/"><img src="https://29c2rwit2ctb68ey.public.blob.vercel-storage.com/write-fJBQtCpZLR2bOKuQpzfYLjFueTtPYQ.ico" alt="logo" className="sm:w-10 sm:h-10 w-8 h-8" /></a>
             <a href="/" className=" sm:text-3xl text-2xl text-blue-500 ">write</a>
